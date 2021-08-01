722b64fa1278e908cccb6e68b2ae4bd3e506a1a5
sebaspv <poncegramming@gmail.com>
2021-08-01 12:12:31 -0500

    FINAL COMMIT

---
+++ app/src/main/java/exportkit/xd/main_activity.java
@@ -0,0 +1,90 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		main
+	 *	@date 		1627811026110
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+package exportkit.xd;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+	public class main_activity extends Activity {
+
+	
+	private View _bg__main_ek2;
+	private ImageView media;
+	private View rectangle_12;
+	private ImageView vector;
+	private View ellipse_15;
+	private TextView log_out;
+	private View backdrop_base;
+	private TextView matt_s_room;
+	private TextView _1_person_inside;
+	private TextView current_o2_level_;
+	private TextView _60_;
+	private TextView camera_view;
+	private ImageView icon;
+	private ImageView icon_ek1;
+	private ImageView icon_ek2;
+	private ImageView background_ek1;
+	private View block;
+	private ImageView vector_ek1;
+	private ImageView vector_ek2;
+	private ImageView vector_ek3;
+	private ImageView vector_ek4;
+	private View block_ek1;
+	private ImageView icon_ek3;
+	private View divider;
+	private ImageView image_6;
+
+	private int person_count = 1;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		
+		_bg__main_ek2 = (View) findViewById(R.id._bg__main_ek2);
+		media = (ImageView) findViewById(R.id.media);
+		rectangle_12 = (View) findViewById(R.id.rectangle_12);
+		vector = (ImageView) findViewById(R.id.vector);
+		ellipse_15 = (View) findViewById(R.id.ellipse_15);
+		log_out = (TextView) findViewById(R.id.log_out);
+		backdrop_base = (View) findViewById(R.id.backdrop_base);
+		matt_s_room = (TextView) findViewById(R.id.matt_s_room);
+		_1_person_inside = (TextView) findViewById(R.id._1_person_inside);
+		current_o2_level_ = (TextView) findViewById(R.id.current_o2_level_);
+		_60_ = (TextView) findViewById(R.id._60_);
+		camera_view = (TextView) findViewById(R.id.camera_view);
+		icon = (ImageView) findViewById(R.id.icon);
+		icon_ek1 = (ImageView) findViewById(R.id.icon_ek1);
+		icon_ek2 = (ImageView) findViewById(R.id.icon_ek2);
+		background_ek1 = (ImageView) findViewById(R.id.background_ek1);
+		block = (View) findViewById(R.id.block);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+		vector_ek3 = (ImageView) findViewById(R.id.vector_ek3);
+		vector_ek4 = (ImageView) findViewById(R.id.vector_ek4);
+		block_ek1 = (View) findViewById(R.id.block_ek1);
+		icon_ek3 = (ImageView) findViewById(R.id.icon_ek3);
+		divider = (View) findViewById(R.id.divider);
+		image_6 = (ImageView) findViewById(R.id.image_6);
+
+	}
+}
+	
+	
